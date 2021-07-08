5120aee23a27a15b33f08f73a786d0707382bc08
Umair Darr <umerderr@gmail.com>
2021-07-08 22:36:29 +0100

    feat: get rid of token code

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,5 +1,3 @@
 export { handleShareTransfer, handleAffiliateShareTransfer } from './mappings/yVault';
 export { handleHarvest } from './mappings/strategy';
 export { handleStaked, handleUnstaked } from './mappings/geyser';
-export { handleFarmHarvest } from './mappings/farm';
-export { handleSushiHarvest } from './mappings/sushi';
